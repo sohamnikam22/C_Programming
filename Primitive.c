18a5f055afb9aaba2dc10bf01fae9841d2f6ec4b
sohamnikam22 <sohamnikam2202@gmail.com>
2026-04-24 20:17:25 +0530

    Add files via upload

---
+++ Primitive.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char ch = 'A';
+    int i = 11;
+    float f = 90.99f;
+    double d = 78.67890;
+
+    printf("%c\n",ch);
+    printf("%d\n",i);
+    printf("%f\n",f);
+    printf("%lf\n",d);
+    return 0;
+}
