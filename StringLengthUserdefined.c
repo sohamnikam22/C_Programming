c4cadd76c53342097d6872e75bef46bbf29fec4c
sohamnikam22 <sohamnikam2202@gmail.com>
2026-05-04 22:05:06 +0530

    Add files via upload

---
+++ StringLengthUserdefined.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+// Call by Address
+int strlenX(char *ptr)
+{
+    int i = 0;
+
+    while(*ptr != '\0')
+    {
+       i++;
+       ptr++;
+
+    }
+
+    return i;
+
+}
+
+int main()
+{
+    char str[] = "Ganesh";    // 100
+    int ret = 0;
+    
+    ret = strlenX(str);
+
+    printf("String length is : %d\n",ret);
+    
+    return 0;
+}
